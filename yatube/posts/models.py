04f6370f3546bc22206202a1b04da751d4556b65
Nikolay Dubinin <dubinin.kerch@yandex.ru>
2022-11-21 16:41:13 +0300

    Transfer from laptop to PC commit

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,6 +6,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.TextField()
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -14,3 +23,14 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+    group = models.ForeignKey(
+        Group,
+        blank = True,
+        null = True,
+        on_delete = models.CASCADE,
+        related_name = 'posts',
+    )
+
+
+
+
--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -9,6 +9,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('group/<slug:pk>/', views.group_posts, name='group_posts'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
     path('', views.index, name='index'),
 ]
