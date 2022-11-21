1c33e0c8595ec14b90f55baa9df4f569dc1bfd82
Nikolay Dubinin <dubinin.kerch@yandex.ru>
2022-11-21 17:21:35 +0300

    First commit for code review

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -7,17 +7,21 @@ from .models import Post, Group
 class PostAdmin(admin.ModelAdmin):
     # Перечислчем поля, которые должны отображаться в админке
     list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    # Добавляем возможность редактирования группы
+    list_editable = ('group',)
     # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
     # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
     # Это свойство сработает для всех колонок: где пусто — там будет эта строка
     empty_value_display = '-пусто-'
-    list_editable = ('group',)
+    
 
 
 class GroupAdmin(admin.ModelAdmin):
+    # Перечислчем поля, которые должны отображаться в админке
     list_display = ('title', 'slug', 'description')
+    # Добавляем интерфейс для поиска по названию группы
     search_fields = ('title',)
     # Это свойство сработает для всех колонок: где пусто — там будет эта строка
     empty_value_display = '-пусто-'
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -8,7 +8,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.TextField()
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self) -> str:
